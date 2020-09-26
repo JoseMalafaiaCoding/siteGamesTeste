3051a348e5410df0955c56ffaaa4d037617b9b1d
Jose Malafaia Code <jose.malafaiacode@outlook.com>
2020-09-26 15:44:20 -0300

    Send to github

---
+++ _MySQL/siteGamesQuery.sql
@@ -0,0 +1,5 @@
+update jogos set descricao = 'Persona 5 é um JRPG baseado em turnos da aclamada série desenvolvida pela ATLUS, na aventura o jogador assume o papel de um jovem estudante que foi injustamente posto em uma probação por um crime que não cometeu e ao longo dessa jornada épica coisas, estranhas ocorrem em seus sonhos e na realidade e ele precisa se aliar a várias pessoas pessoas na cidade de Tokyo para enfrentar seus desafios.'
+where idJogo = '1';
+
+select * from jogos;
+
